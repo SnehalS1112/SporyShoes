2943e886b80b5800f5390dff15fd61c0176c98c5
SnehalS1112 <101574725+SnehalS1112@users.noreply.github.com>
2022-07-17 21:07:14 +0530

    Update SearchController.java

--- java/com/api/sportyShoes/controller/SearchController.java
+++ java/com/api/sportyShoes/controller/SearchController.java
@@ -20,10 +20,7 @@ public class SearchController {
 	@Autowired
 	private SportyShoesService service;
 	
-	/**
-	 * Shoe search controller
-	 * @return all shoe list
-	 */
+
 	
 	@GetMapping("/admin/shoe/all")
 	public ResponseEntity<List<Shoe>> getAllShoes(){
@@ -31,31 +28,20 @@ public class SearchController {
 	}
 	
 	
-	/**
-	 * Purchase Report Search Controller
-	 * @param category
-	 * @return purchase reports filtered by the category
-	 */
+	
 	@GetMapping("/admin/purchaseReport/category/{category}")
 	public ResponseEntity<List<PurchaseReport>> getAllPurchaseReportsByCategory(@PathVariable String category){
 		return new ResponseEntity<List<PurchaseReport>>(service.getAllPurchaseReportsByCategory(category), HttpStatus.OK);
 	}
 	
-	/**
-	 * Purchase Report Search Controller
-	 * @param dateInMs
-	 * @return purchase reports filtered by date of purchase(in millisecond time)
-	 */
+	
 	@GetMapping("/admin/purchaseReport/date/{dateInMs}")
 	public ResponseEntity<List<PurchaseReport>> getAllPurchaseReportsByDop(@PathVariable Long dateInMs){
 		Date dop = new Date(dateInMs);
 		return new ResponseEntity<List<PurchaseReport>>(service.getAllPurchaseReportsByDOP(dop), HttpStatus.OK);
 	}
 	
-	/**
-	 * Purchase Report Search Controller
-	 * @return all purchase reports
-	 */
+	
 	@GetMapping("/admin/purchaseReport/all")
 	public ResponseEntity<List<PurchaseReport>> getAllPurchaseReport(){
 		return new ResponseEntity<List<PurchaseReport>>(service.getAllPurchaseReports(), HttpStatus.OK);
